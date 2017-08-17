c34b2769523589aa94574c0ca08e9542a5d81723
Richard Savard <richard.savard.ca@gmail.com>
2017-08-16 22:02:17 -0400

    test commit

--- service/urls.py
+++ service/urls.py
@@ -4,12 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-<<<<<<< HEAD
-    url(r"", ),
-dfdfdfsf
-dfdsds
-
-=======
     url(
->>>>>>> 1bc0f50feeaae3a52f27df058985c66ffa590652
 ]
